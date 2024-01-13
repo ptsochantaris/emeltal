15ea35fa3142ea1aa93f5833e2803065c0153e70
Paul Tsochantaris <ptsochantaris@icloud.com>
2024-01-13 11:42:09 +0000

    Tidying

--- Emeltal/WhisperContext.swift
+++ Emeltal/WhisperContext.swift
@@ -1,29 +1,5 @@
 import Foundation
 
-final class GGMLExecutor: SerialExecutor {
-    static let shared = GGMLExecutor()
-
-    private let ggmlQueue = DispatchQueue(label: "build.bru.emeltal.ggml", qos: .userInitiated)
-
-    func enqueue(_ job: consuming ExecutorJob) {
-        let j = UnownedJob(job)
-        let e = asUnownedSerialExecutor()
-        ggmlQueue.async {
-            j.runSynchronously(on: e)
-        }
-    }
-}
-
-@globalActor actor GGMLActor {
-    static var shared = GGMLActor()
-
-    static var sharedUnownedExecutor = GGMLExecutor.shared.asUnownedSerialExecutor()
-
-    nonisolated var unownedExecutor: UnownedSerialExecutor {
-        Self.sharedUnownedExecutor
-    }
-}
-
 @GGMLActor
 final class WhisperContext {
     private var context: OpaquePointer
