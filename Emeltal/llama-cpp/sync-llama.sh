4fa385ecd99096b75f91a2964dd9b1f0d73adcf2
Paul Tsochantaris <ptsochantaris@icloud.com>
2024-02-16 21:56:29 +0000

    Library updates, sync script for llama and whisper

---
+++ Emeltal/llama-cpp/sync-llama.sh
@@ -0,0 +1,25 @@
+LSRC=~/llama.cpp
+WSRC=~/whisper.cpp
+
+cp\
+ $LSRC/ggml-alloc.c\
+ $LSRC/ggml-alloc.h\
+ $LSRC/ggml-backend-impl.h\
+ $LSRC/ggml-backend.c\
+ $LSRC/ggml-backend.h\
+ $LSRC/ggml-impl.h\
+ $LSRC/ggml-metal.h\
+ $LSRC/ggml-metal.m\
+ $LSRC/ggml-metal.metal\
+ $LSRC/ggml-quants.c\
+ $LSRC/ggml-quants.h\
+ $LSRC/ggml.c\
+ $LSRC/ggml.h\
+ $LSRC/llama.cpp\
+ $LSRC/llama.h\
+ $LSRC/unicode.h\
+ $WSRC/whisper.cpp\
+ $WSRC/whisper.h\
+ .
+
+patch ggml-metal.m < ggml-metal.diff
