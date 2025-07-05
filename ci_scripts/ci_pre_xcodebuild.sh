c38984955bad0181b50d58995ef25415a2ce7ac2
Paul Tsochantaris <ptsochantaris@icloud.com>
2025-07-06 00:23:40 +0100

    CI Script tweak

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 if [ "$CI_WORKFLOW_ID" = "AB4F8343-1ADC-40C0-964A-0DDC07E7A2D5" ]; then
-    xcodebuild -downloadComponent metalToolchain -exportPath /tmp/MyMetalExport/
-    sed -i '' -e 's/17A5241c/17A5241e/g' /tmp/MyMetalExport/MetalToolchain-17A5241c.exportedBundle/ExportMetadata.plist
-    xcodebuild -importComponent metalToolchain -importPath /tmp/MyMetalExport/MetalToolchain-17A5241c.exportedBundle
+    xcodebuild -downloadComponent metalToolchain
 fi
