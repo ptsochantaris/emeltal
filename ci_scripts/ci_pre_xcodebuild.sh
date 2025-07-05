e3898c02a2dc72d045af1a0979a70a1bcc0f3a91
Paul Tsochantaris <ptsochantaris@icloud.com>
2025-07-05 17:23:08 +0100

    Metal build script tweak

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$CI_WORKFLOW_ID" = "AB4F8343-1ADC-40C0-964A-0DDC07E7A2D5" ]; then
-    xcodebuild -downloadComponent metalToolchain -exportPath /tmp/MyMetalExport/
-    sed -i '' -e 's/17A5241c/17A5241e/g' /tmp/MyMetalExport/MetalToolchain-17A5241c.exportedBundle/ExportMetadata.plist
-    xcodebuild -importComponent metalToolchain -importPath /tmp/MyMetalExport/MetalToolchain-17A5241c.exportedBundle
+    xcodebuild -downloadComponent metalToolchain -exportPath $CI_WORKSPACE_PATH/MyMetalExport/
+    sed -i '' -e 's/17A5241c/17A5241e/g' $CI_WORKSPACE_PATH/MyMetalExport/MetalToolchain-17A5241c.exportedBundle/ExportMetadata.plist
+    xcodebuild -importComponent metalToolchain -importPath $CI_WORKSPACE_PATH/MyMetalExport/MetalToolchain-17A5241c.exportedBundle
 fi
